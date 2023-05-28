5080eda072e6c724c01c1e1bc5fc0ca67d1d163c
Vinayavenkat <r.v.vinaya@gmai.com>
2023-05-28 19:15:25 -0400

    recent changes for netlifybuild

--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -3,14 +3,17 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
 import TodoList from "./todo/todo-list";
+import TodoItem from "./todo/todo-item";
 function Assignment3() {
  return (
    <div>
      <h1>Assignment 3</h1>
+     <TodoItem/>
      <ConditionalOutput/>
      <Styles/>
      <Classes/>
      <JavaScript/>
+     
      <TodoList/>
    </div>
  );
