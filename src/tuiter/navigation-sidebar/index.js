a2c67b170414867618459c026625634b57dfb04a
Vinayavenkat <r.v.vinaya@gmai.com>
2023-05-28 19:20:21 -0400

    added ignore,tuiter in nav side bar

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 const NavigationSidebar = () => {
  const { pathname } = useLocation();
- const [active] = pathname.split("/");
+ const [ignore, tuiter, active] = pathname.split("/");
  const links = ["home",     "explore",   "notifications", "messages", "bookmarks", "lists", "profile",  "more"];
  return (
    <div className="list-group">
