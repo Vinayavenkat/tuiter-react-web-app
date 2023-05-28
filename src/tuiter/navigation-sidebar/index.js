4e22291eb5180f89c08bf95e027251c8a7047b53
Vinayavenkat <r.v.vinaya@gmai.com>
2023-05-28 19:24:56 -0400

    index.js small change for netlify deploy

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+
 const NavigationSidebar = () => {
- const { pathname } = useLocation();
- const [ignore, tuiter, active] = pathname.split("/");
- const links = ["home",     "explore",   "notifications", "messages", "bookmarks", "lists", "profile",  "more"];
- return (
-   <div className="list-group">
-     {links.map((link) => 
-         <Link to={`/tuiter/${link}`} className={`list-group-item text-capitalize ${active === link ? "active" : ""}`}>
-           {link}
-         </Link>
-     )}
-   </div>
- );
+  const { pathname } = useLocation();
+  const [ignore, tuiter, active] = pathname.split("/");
+  const links = ["home", "explore", "notifications", "messages", "bookmarks", "lists", "profile", "more"];
+
+  return (
+    <div className="list-group">
+      {links.map((link) => (
+        <Link
+          to={`/tuiter/${link}`}
+          className={`list-group-item text-capitalize ${active === link ? "active" : ""}`}
+        >
+          {link}
+        </Link>
+      ))}
+      <p className="text-white fs-6">{ignore}</p>
+      <p className="text-white fs-6">{tuiter}</p>
+    </div>
+    
+  );
+  
 };
+
+
 export default NavigationSidebar;
