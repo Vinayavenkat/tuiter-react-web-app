c32ddf93ed7e8ce413ac3f6854beabf8028b1836
Vinaya <vinaya@kayatech.com>
2023-06-16 21:01:48 -0500

    small changes

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -2,46 +2,31 @@ import axios from "axios";
 const USERS_URL = "https://tuiter-node-server-app-mongo-6i0j.onrender.com/api/users";
 
 
-
+console.log(USERS_URL);
 const api = axios.create({ withCredentials: true });
 
+
+export const login = async ({ username, password }) => {
+ const response = await api.post(`${USERS_URL}/login`, { username, password });
+ const user = response.data;
+ return user;
+};
 
 export const logout = async () => {
     const response = await api.post(`${USERS_URL}/logout`);
     return response.data;
-  };
-  
-  export const profile = async () => {
+   };
+export const profile = async () => {
     const response = await api.post(`${USERS_URL}/profile`);
     return response.data;
-  };
-  
-  export const updateUser = async (user) => {
+   };
+export const updateUser = async (user) => {
     const response = await api.put(`${USERS_URL}/${user._id}`, user);
     return response.data;
-  };
-  
-  export const register = async ({ username, password}) => {
-    const userData = { username, password};
-  
-    try {
-      const response = await api.post(`${USERS_URL}/register`, userData);
-      return response.data;
-    } catch (error) {
-      throw new Error('Registration failed. Please try again.');
-    }
-  };
-  
-  
-  export const login = async ({ username, password }) => {
-    const userData = { username, password };
-  
-    try {
-      const response = await api.post(`${USERS_URL}/login`, userData);
-      return response.data;
-    } catch (error) {
-      throw new Error('Login failed. Please check your credentials and try again.');
-    }
-  };
-  
-  
+   };
+export const register = async ({ username, password }) => {
+    const response = await api.post(`${USERS_URL}/register`, { username, password });
+    const user = response.data;
+    console.log(user);
+    return user;
+};
